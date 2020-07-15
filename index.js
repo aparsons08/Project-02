23340e282add0ca7f7c22a8250e672916c75a218
Angela Parsons <angelaparsons76@yahoo.com>
2020-07-15 09:40:40 -0700

    Complet jquery part of project

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ function searchFromGiphy(query) {
         }
     })
 }
+
+function showResult(result) {
+    console.log(result)
+    let data = result.data;
+
+    let result_html = "";
+
+    data.forEach(gif => {
+        let div =
+        <div class="custom-grid">
+        <img src="${gif.images.downsized.url}">
+        </div>
+    ;
+        result_html =result_html + div 
+
+    })
+    $('#gif-count').val(data.length)
+    $('.search-contents').html('')
+    $('.search-contents').append(result_html)
+
+}
