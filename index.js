85c97ba0e019538282274ee454aeaae727d4d990
Angela Parsons <angelaparsons76@yahoo.com>
2020-07-15 09:26:18 -0700

    Entered gif search event

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const GiphyApi = 'http://api.giphy.com/v1/gifs/search?apiKey=';
 const GiphyApikey = '';
+
+$(document).keyup(function (event) {
+    if ($("#gif-search").is(":focus")&& event.key == "Enter") {
+        //Do work
+        console.log('do work')
+        let squery = $('#gif-search').val()
+        console.log(squery)
+        searchFromGiphy(squery)
+    }
+});
+
